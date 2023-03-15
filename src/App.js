0631b161edfe16f77df687cead78c059ca6e880c
Le The Tiem <19522330@gm.uit.edu.vn>
2023-03-15 11:28:30 +0700

    demon tets

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './components/BasicForms'
+import BasicForms from "./components/BasicForms";
+
+
+const App = () =>{
+  return <div className="app">
+    <BasicForms></BasicForms>
+  </div>
+}
+
+export default App;
